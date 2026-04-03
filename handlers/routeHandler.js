90044bd345a972f232f0f81b95f71ed19d2696e7
Hammad Ullah <125109241+Haadii-dev@users.noreply.github.com>
2026-04-03 23:33:27 +0100

    Handled POST and GET

---
+++ handlers/routeHandler.js
@@ -0,0 +1,12 @@
+import { getData } from '../utils/getData.js'
+import { sendResponse } from '../utils/sendResponse.js'
+
+export async function handleGet(res) {
+  const data = await getData()
+  const content = JSON.stringify(data)
+  sendResponse(res, 200, 'application/json', content)
+}  
+
+export function handlePost(req, res) {
+  console.log('POST request received')
+} 
